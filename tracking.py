acbfa7f4607dc6c3cba0de4635755d9d3857086e
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-08-08 17:08:30 -0700

    Fixed warning SettingWithCopyWarning by adding .copy() explicitly.

--- tracking.py
+++ tracking.py
@@ -3,7 +3,8 @@ import numpy as np
 
 # TODO: fix warning
 # make friendly comments to user when input / output files can't be open
-# 
+# delete extra columns 
+
 file_name = 'smple tracking.xlsx'
 
 try:
@@ -23,29 +24,30 @@ new_headers = [f'Tracking Number {i}' for i in range(2, 8)]
 data = data.reindex(columns = data.columns.tolist() + new_headers)
 
 # Create merged dataframe by adding the first customer
-#merged = data.iloc[[0]]
-#all_orders = data.loc[data['Customer Email'] == merged.loc[0, 'Customer Email']]
-#
-## Insert additional tracking numbers for the first customer
-#for i, number in enumerate(all_orders.loc[1:,'Tracking Number']):
-#    merged.iloc[0, i+3] = number
-#
-#for index, order in data.iterrows():
-#    email = order['Customer Email']
-#    # Make sure that the list is filtered only once on each email
-#    if  email not in merged.loc[:, 'Customer Email'].unique():
-#        all_orders = data.loc[data['Customer Email'] == order.loc['Customer Email']]
-#        for i, number in enumerate(all_orders.loc[1:,'Tracking Number']):
-#            order.iloc[i+2] = number
-#        merged = merged.append(order)
-#
-#merged = merged.rename(index=str, columns={'To Name':'Name', 
-#                                    'Customer Email':'Email',
-#                                    'Tracking Number':'Tracking Number 1',
-#                                    })
+merged = data.iloc[[0]].copy()
+all_orders = data.loc[data['Customer Email'] == merged.loc[0, 'Customer Email']]
+
+# Insert additional tracking numbers for the first customer
+for i, number in enumerate(all_orders.loc[1:,'Tracking Number']):
+#merged.iloc[0, i+3] = number
+    merged.loc[merged.index[0], f'Tracking Number {i+2}'] = number
+
+for index, order in data.iterrows():
+    email = order['Customer Email']
+    # Make sure that the list is filtered only once on each email
+    if  email not in merged.loc[:, 'Customer Email'].unique():
+        all_orders = data.loc[data['Customer Email'] == order.loc['Customer Email']]
+        for i, number in enumerate(all_orders.loc[1:,'Tracking Number']):
+            order.iloc[i+2] = number
+        merged = merged.append(order)
+
+merged = merged.rename(index=str, columns={'To Name':'Name', 
+                                    'Customer Email':'Email',
+                                    'Tracking Number':'Tracking Number 1',
+                                    })
 
 merged.set_index('Name', inplace=True)
-#print(merged)
+print(merged)
 out_file_name = 'merged_tracking.csv'
 try:
     merged.to_csv(out_file_name)
